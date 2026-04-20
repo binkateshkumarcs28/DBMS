e70006a7013d6bdcb7026a467b63aa5330d1c166
Binkatesh kumar <binkatesh.kumar.cs28@iilm.edu>
2026-04-20 12:10:26 +0530

    Add files via upload

---
+++ LAB_Practicals/EXP_12.sql
@@ -0,0 +1,13 @@
+        -- Experiment - 12
+USE company2CSE2;
+
+-- 1. Display those employees whose salary is less than his manager but more than salary of any other managers.
+-- 2. Find out the number of employees whose salary is greater than their manager salary?
+-- 3. Display those managers who are not working under president but they are working under any other manager?
+-- 4. Delete those department where no employee working?
+-- 5. Delete those records from emp table whose deptno not available in dept table?
+-- 6. Display those earners whose salary is out of the grade available in sal grade table?
+-- 7. Display employee name, sal, comm. And whose net pay is greater than any other in the company?
+-- 8. Display those employees who are working in sales or research?
+-- 9. Display the grade of jones?
+-- 10. Display the department name the no of characters of which is equal to no of employees in any other department?
